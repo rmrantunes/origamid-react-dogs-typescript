3560ec2e2d530b9cbf397cecc21c0eeb31527767
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-07 11:12:33 -0400

    Log user is function now come from UserContext

--- src/core/components/Login/LoginForm.tsx
+++ src/core/components/Login/LoginForm.tsx
@@ -1,44 +1,18 @@
-import { FormEvent, useEffect } from "react";
+import { FormEvent, useContext } from "react";
 import { Link } from "react-router-dom";
-import {
-  TokenResponse,
-  TOKEN_POST,
-  UserResponse,
-  USER_GET,
-} from "src/adapters";
 import { Input, Button } from "src/core/components";
+import { UserContext } from "src/core/contexts";
 import { useForm } from "src/core/hooks";
 
 export const LoginForm = () => {
   const username = useForm();
   const password = useForm();
-
-  useEffect(() => {
-    const token = window.localStorage.getItem("token");
-    if (token) getUser(token);
-  }, []);
-
-  async function getUser(token: string) {
-    const { url, options } = USER_GET(token);
-    const response = await fetch(url, options);
-    const user: UserResponse = await response.json();
-  }
+  const { loginUser } = useContext(UserContext);
 
   async function handleLogin(event: FormEvent) {
     event.preventDefault();
-
     if (!username.validate() || !password.validate()) return;
-
-    const { url, options } = TOKEN_POST({
-      username: username.value,
-      password: password.value,
-    });
-
-    const response = await fetch(url, options);
-    const login: TokenResponse = await response.json();
-
-    getUser(login.token);
-    window.localStorage.setItem("token", login.token);
+    loginUser(username.value, password.value);
   }
 
   return (
