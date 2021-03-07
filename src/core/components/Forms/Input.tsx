e7d4ad21010c9951cd84ee10f5a7693a246f39d3
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-07 09:44:00 -0400

    useForm hook implementation in LoginForm and Input components

--- src/core/components/Forms/Input.tsx
+++ src/core/components/Forms/Input.tsx
@@ -4,16 +4,33 @@ interface InputProps {
   label: string;
   type?: string;
   name: string;
+  value: string;
+  error: string | null;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+  onChange: ({ target }: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur: () => boolean;
 }
 
-export const Input = ({ label, type = "text", name }: InputProps) => {
+export const Input = ({
+  label,
+  type = "text",
+  name,
+  value,
+  error,
+  onChange,
+  onBlur,
+}: InputProps) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input id={name} className={styles.input} {...{ type, name }} />
-      <p className={styles.error}>Error</p>
+      <input
+        id={name}
+        className={styles.input}
+        {...{ type, name, onChange, value, onBlur }}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };
--- src/core/components/Login/LoginForm.tsx
+++ src/core/components/Login/LoginForm.tsx
@@ -1,17 +1,21 @@
-import { FormEvent, useState } from "react";
+import { FormEvent } from "react";
 import { Link } from "react-router-dom";
 import { Input, Button } from "src/core/components";
+import { useForm } from "src/core/hooks";
 
 export const LoginForm = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const username = useForm();
+  const password = useForm();
 
   function handleLogin(event: FormEvent) {
     event.preventDefault();
+
+    if (!username.validate() && !password.validate()) return;
+
     fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({}),
     })
       .then((response) => {
         console.log(response);
@@ -24,8 +28,8 @@ export const LoginForm = () => {
     <section>
       <h1>Login</h1>
       <form onSubmit={handleLogin}>
-        <Input name="username" label="Usuário" />
-        <Input name="password" type="password" label="Senha" />
+        <Input name="username" label="Usuário" {...username} />
+        <Input name="password" type="password" label="Senha" {...password} />
         <Button>Entrar</Button>
       </form>
       <Link to="/login/create">Cadastrar</Link>
