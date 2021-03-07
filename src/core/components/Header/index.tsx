9b907965b1a3f69891bd7cdb12ef514e74a3b163
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-07 11:13:02 -0400

    Using UserContext values in Header

--- src/core/components/Header/index.tsx
+++ src/core/components/Header/index.tsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "./Header.module.css";
 
 import { ReactComponent as Dogs } from "src/assets/dogs.svg";
+import { UserContext } from "src/core/contexts";
 
 export const Header = () => {
+  const { user } = useContext(UserContext);
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link to="/" aria-label="Ir para Início" className={styles.logo}>
           <Dogs />
         </Link>
-        <Link to="/login" className={styles.login}>
-          Login / Criar
-        </Link>
+        {user ? (
+          <Link to="/profile" className={styles.login}>
+            {user.nome}
+          </Link>
+        ) : (
+          <Link to="/login" className={styles.login}>
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
