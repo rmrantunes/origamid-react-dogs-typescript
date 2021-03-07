e29b10b231374ab2d560da42a64a67051f2a7e5e
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-07 08:08:10 -0400

    Login and its nested routes

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+import {
+  LoginForm,
+  LoginCreate,
+  LoginForgotPassword,
+  LoginResetPasswowd,
+} from "src/core/components";
 
 export const Login = () => {
-  return <div>Login</div>;
+  const { path } = useRouteMatch();
+
+  return (
+    <div>
+      <Switch>
+        <Route path={path} exact component={LoginForm} />
+        <Route path={`${path}/create`} component={LoginCreate} />
+        <Route
+          path={`${path}/forgot-password`}
+          component={LoginForgotPassword}
+        />
+        <Route path={`${path}/reset-password`} component={LoginResetPasswowd} />
+      </Switch>
+    </div>
+  );
 };
