53ddf7bbe241c2d1f620a4bb6f47a168a9c8bb8d
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-09 14:43:50 -0400

    Implementation of the Login page styles

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,25 +1,37 @@
-import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { useContext } from "react";
+import { Route, Switch, useRouteMatch, Redirect } from "react-router-dom";
 import {
   LoginForm,
   LoginCreate,
   LoginForgotPassword,
   LoginResetPasswowd,
 } from "src/core/components";
+import { UserContext } from "src/core/contexts";
+
+import styles from "./Login.module.css";
 
 export const Login = () => {
   const { path } = useRouteMatch();
+  const { login } = useContext(UserContext);
+
+  if (login === true) return <Redirect to="/profile" />;
 
   return (
-    <div>
-      <Switch>
-        <Route path={path} exact component={LoginForm} />
-        <Route path={`${path}/create`} component={LoginCreate} />
-        <Route
-          path={`${path}/forgot-password`}
-          component={LoginForgotPassword}
-        />
-        <Route path={`${path}/reset-password`} component={LoginResetPasswowd} />
-      </Switch>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Switch>
+          <Route path={path} exact component={LoginForm} />
+          <Route path={`${path}/create`} component={LoginCreate} />
+          <Route
+            path={`${path}/forgot-password`}
+            component={LoginForgotPassword}
+          />
+          <Route
+            path={`${path}/reset-password`}
+            component={LoginResetPasswowd}
+          />
+        </Switch>
+      </div>
+    </section>
   );
 };
