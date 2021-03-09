22e0b3f87cc963b7bcfab9d3090a00091e02811d
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-09 08:08:33 -0400

    Button for logout user

--- src/core/components/Header/index.tsx
+++ src/core/components/Header/index.tsx
@@ -7,18 +7,22 @@ import { ReactComponent as Dogs } from "src/assets/dogs.svg";
 import { UserContext } from "src/core/contexts";
 
 export const Header = () => {
-  const { user } = useContext(UserContext);
+  const { user, logoutUser } = useContext(UserContext);
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link to="/" aria-label="Ir para Início" className={styles.logo}>
           <Dogs />
         </Link>
-        {user ? (
-          <Link to="/profile" className={styles.login}>
-            {user.nome}
-          </Link>
-        ) : (
+        {user.nome && (
+          <>
+            <Link to="/profile" className={styles.login}>
+              {user.nome}
+            </Link>
+            <button onClick={logoutUser}>Sair</button>
+          </>
+        )}
+        {!user.nome && (
           <Link to="/login" className={styles.login}>
             Login / Criar
           </Link>
