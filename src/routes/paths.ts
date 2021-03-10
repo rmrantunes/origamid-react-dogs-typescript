2d6ea46d13e75626c1f401ed84700bec7fec81f8
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-10 10:26:48 -0400

    New paths: LOGIN_PATHS

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -2,6 +2,12 @@ export const ROOT = "/";
 
 export const LOGIN_PATH = "/login";
 
+export const LOGIN_PATHS = {
+  CREATE: LOGIN_PATH + "/create",
+  FORGOT_PASSWORD: LOGIN_PATH + "/forgot-password",
+  RESET_PASSWORD: LOGIN_PATH + "/reset-password",
+};
+
 export const PROFILE_PATH = "/profile";
 
 export const PROFILE_PATHS = {
