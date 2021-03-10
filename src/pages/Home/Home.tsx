ccb179a4ff523be42837779ff9876d46c7c97424
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-10 19:08:42 -0400

    Home page markup and styles

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,9 @@
+import { Feed } from "src/core/components";
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="container mainContainer">
+      <Feed />
+    </section>
+  );
 };
