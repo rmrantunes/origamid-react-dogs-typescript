767c9dcf1559302596cda49f703a1950eaaba76f
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-10 11:31:44 -0400

    Implementing path constants in Header links

--- src/core/components/Header/Header.tsx
+++ src/core/components/Header/Header.tsx
@@ -5,22 +5,23 @@ import styles from "./Header.module.css";
 
 import { ReactComponent as Dogs } from "src/assets/dogs.svg";
 import { UserContext } from "src/core/contexts";
+import { ROOT, PROFILE_PATH, LOGIN_PATH } from "src/routes/paths";
 
 export const Header = () => {
   const { user } = useContext(UserContext);
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
-        <Link to="/" aria-label="Ir para Início" className={styles.logo}>
+        <Link to={ROOT} aria-label="Ir para Início" className={styles.logo}>
           <Dogs />
         </Link>
         {user.nome && (
-          <Link to="/profile" className={styles.login}>
+          <Link to={PROFILE_PATH} className={styles.login}>
             {user.nome}
           </Link>
         )}
         {!user.nome && (
-          <Link to="/login" className={styles.login}>
+          <Link to={LOGIN_PATH} className={styles.login}>
             Login / Criar
           </Link>
         )}
