9d98dcada3d1f7ddb0a4afa6e60eecd1cecf6878
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-10 10:15:42 -0400

    Organizing paths as constants

---
+++ src/routes/paths.ts
@@ -0,0 +1,11 @@
+export const ROOT = "/";
+
+export const LOGIN_PATH = "/login";
+
+export const PROFILE_PATH = "/profile";
+
+export const PROFILE_PATHS = {
+  MY_PHOTOS: PROFILE_PATH,
+  POST_PHOTO: PROFILE_PATH + "/post-photo",
+  STATS: PROFILE_PATH + "/stats",
+};
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,16 +2,18 @@ import { useContext } from "react";
 import { Switch, Route } from "react-router-dom";
 import { UserContext } from "src/core/contexts";
 import { Home, Login, User } from "src/pages";
+
 import { ProtectedRoute } from "src/routes/ProtectedRoute";
+import { ROOT, LOGIN_PATH, PROFILE_PATH } from "src/routes/paths";
 
 export const Routes = () => {
   const { login } = useContext(UserContext);
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/login" component={Login} />
-      <ProtectedRoute condition={login} redirectPath="/login">
-        <Route path="/profile" component={User} />
+      <Route path={ROOT} exact component={Home} />
+      <Route path={LOGIN_PATH} component={Login} />
+      <ProtectedRoute condition={login} redirectPath={LOGIN_PATH}>
+        <Route path={PROFILE_PATH} component={User} />
       </ProtectedRoute>
     </Switch>
   );
