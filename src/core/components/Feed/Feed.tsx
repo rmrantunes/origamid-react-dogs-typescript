8454d1ccb0c9de97a8e1e480329e18ebcf2b8bdb
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-11 14:59:20 -0400

    Feed modal photo behavior implementation

--- src/core/components/Feed/Feed.tsx
+++ src/core/components/Feed/Feed.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+import { Photo } from "src/adapters";
 import { FeedModal, FeedPhotos } from "src/core/components";
 
+export interface FeedChildrenSharedProps {
+  setModalPhoto: React.Dispatch<React.SetStateAction<Photo | null>>;
+}
+
 export const Feed = () => {
+  const [modalPhoto, setModalPhoto] = useState<Photo | null>(null);
+
   return (
     <div>
-      <FeedModal />
-      <FeedPhotos />
+      {modalPhoto && <FeedModal photoId={modalPhoto.id} />}
+      <FeedPhotos {...{ setModalPhoto }} />
     </div>
   );
 };
--- src/core/components/Feed/FeedModal.tsx
+++ src/core/components/Feed/FeedModal.tsx
@@ -1,3 +1,40 @@
-export const FeedModal = () => {
-  return <div></div>;
+import { useEffect } from "react";
+import {
+  GET_PHOTO_WITH_COMMENTS_FETCH_CONFIG,
+  PhotoWithComments,
+} from "src/adapters";
+import { useFetch } from "src/core/hooks";
+import { ErrorMessage, Loading, PhotoContent } from "src/core/components";
+
+import styles from "./FeedModal.module.css";
+
+interface FeedModalProps {
+  photoId: number;
+}
+
+/** Every time `FeedModal` renders, it fetches the photo with comments by the photo id */
+export const FeedModal = ({ photoId }: FeedModalProps) => {
+  const {
+    error,
+    request,
+    loading,
+    data: photoWithComments,
+  } = useFetch<PhotoWithComments>();
+
+  useEffect(() => {
+    async function fetchPhotoWithComments() {
+      const { url, options } = GET_PHOTO_WITH_COMMENTS_FETCH_CONFIG(photoId);
+      await request(url, options);
+    }
+
+    fetchPhotoWithComments();
+  }, [photoId, request]);
+
+  return (
+    <div className={styles.modal}>
+      {error && <ErrorMessage {...{ error }} />}
+      {loading && <Loading />}
+      {photoWithComments && <PhotoContent {...{ photoWithComments }} />}
+    </div>
+  );
 };
