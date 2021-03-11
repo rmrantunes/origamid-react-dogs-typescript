3e83f617d5502b96495eef84214ab5430f1d308c
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-11 15:00:43 -0400

    Handle change photo in modal when clicking FeedPhotosItem

--- src/core/components/Feed/FeedPhotos.tsx
+++ src/core/components/Feed/FeedPhotos.tsx
@@ -1,13 +1,22 @@
 import { useEffect } from "react";
 
 import { GET_PHOTOS_FETCH_CONFIG, Photo } from "src/adapters";
-import { FeedPhotosItem, ErrorMessage, Loading } from "src/core/components";
+import {
+  FeedPhotosItem,
+  ErrorMessage,
+  Loading,
+  FeedChildrenSharedProps,
+} from "src/core/components";
 import { useFetch } from "src/core/hooks";
 
 import styles from "./FeedPhotos.module.css";
 
-export const FeedPhotos = () => {
+interface FeedPhotosProps extends FeedChildrenSharedProps {}
+
+export const FeedPhotos = ({ setModalPhoto }: FeedPhotosProps) => {
   const { data: photos, loading, request, error } = useFetch<Photo[]>();
+
+
   useEffect(() => {
     async function fetchPhotos() {
       const { url, options } = GET_PHOTOS_FETCH_CONFIG({
@@ -29,7 +38,7 @@ export const FeedPhotos = () => {
       <div>
         <ul className={styles.photosList + " animeLeft"}>
           {photos.map((photo) => (
-            <FeedPhotosItem {...{ photo }} key={photo.id} />
+            <FeedPhotosItem {...{ photo, setModalPhoto }} key={photo.id} />
           ))}
         </ul>
       </div>
--- src/core/components/Feed/FeedPhotosItem.tsx
+++ src/core/components/Feed/FeedPhotosItem.tsx
@@ -1,16 +1,24 @@
 import { Photo } from "src/adapters";
+import { FeedChildrenSharedProps } from "src/core/components";
 
 import styles from "./FeedPhotosItem.module.css";
 
-interface FeedPhotosItemProps {
+interface FeedPhotosItemProps extends FeedChildrenSharedProps {
   photo: Photo;
 }
 
-export const FeedPhotosItem = ({ photo }: FeedPhotosItemProps) => {
+export const FeedPhotosItem = ({
+  photo,
+  setModalPhoto,
+}: FeedPhotosItemProps) => {
+  function changePhotoInModal() {
+    setModalPhoto(photo);
+  }
+
   return (
-    <li className={styles.photo}>
+    <li className={styles.photo} onClick={changePhotoInModal}>
       <img src={photo.src} alt={photo.title} />
-      <span className={styles.views} >{photo.acessos}</span>
+      <span className={styles.views}>{photo.acessos}</span>
     </li>
   );
 };
