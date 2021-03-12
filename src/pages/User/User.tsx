7613a91a5be913f920c38d71c7f9811fd752dea7
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-12 14:53:56 -0400

    Feed optional prop for showing only logged user's photos

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import { Route, Switch } from "react-router-dom";
 import {
   Feed,
@@ -5,14 +6,19 @@ import {
   UserPhotoPost,
   UserStats,
 } from "src/core/components";
+import { UserContext } from "src/core/contexts";
 import { MY_ACCOUNT_PATHS, MY_ACCOUNT } from "src/routes/paths";
 
 export const User = () => {
+  const { user } = useContext(UserContext);
+
   return (
     <section className="container">
       <UserHeader />
       <Switch>
-        <Route path={MY_ACCOUNT} exact component={Feed} />
+        <Route path={MY_ACCOUNT} exact>
+          <Feed userId={user.id} />
+        </Route>
         <Route path={MY_ACCOUNT_PATHS.POST_PHOTO} component={UserPhotoPost} />
         <Route path={MY_ACCOUNT_PATHS.STATS} component={UserStats} />
       </Switch>
