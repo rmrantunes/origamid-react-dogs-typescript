0357031a5ac795910f2b11e185628dfdd686dcdc
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-13 14:32:42 -0400

    New page: Not found

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
-export * from "./Login";
 export * from "./Home";
+export * from "./Login";
+export * from "./NotFound";
 export * from "./Photo";
 export * from "./Profile";
 export * from "./User";
