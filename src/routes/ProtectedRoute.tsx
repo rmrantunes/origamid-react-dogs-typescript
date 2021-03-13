8d6ce29b4da97b5786f51995046cd7ca3a6ff327
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-13 14:36:25 -0400

    ProtectedRoute improvements for solving 404 issue

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,25 +1,30 @@
 import { ReactNode } from "react";
-import { Redirect } from "react-router";
+import { Redirect, Route, RouteProps, Switch } from "react-router";
 import { HOME } from "src/routes/paths";
 
-interface ProtectedRouteProps {
+interface ProtectedRouteProps extends RouteProps {
   redirectPath?: string;
-  /** Null for idle state. For example: when getting token on localStorage */
+  /** Null for idle state. For example: when getting token on localStorage. */
   condition: boolean | null;
-  /** This will be shown when `condition` is `null` */
+  /** This will be shown when @property `condition` is `null`. For expample: `<Loading />` */
   fallback?: ReactNode;
 }
 
 export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
-  children,
   redirectPath,
   condition,
   fallback,
+  ...routeProps
 }) => {
   const hasAccess = condition === true;
   const forbiddenAccess = condition === false;
 
-  if (hasAccess) return <>{children}</>;
+  if (hasAccess)
+    return (
+      <Switch>
+        <Route {...routeProps} />
+      </Switch>
+    );
   if (forbiddenAccess) return <Redirect to={redirectPath || HOME} />;
   return fallback ? <>{fallback}</> : <></>;
 };
