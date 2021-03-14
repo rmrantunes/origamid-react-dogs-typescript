e24574e97b8d538bc3ed8b24ff9b88fc83de33d3
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-14 11:21:19 -0400

    Minor code fix to ./Footer

--- src/core/components/index.ts
+++ src/core/components/index.ts
@@ -1,6 +1,6 @@
 export * from "./Feed";
 export * from "./Feedbacks";
-export * from "./Footer/Footer";
+export * from "./Footer";
 export * from "./Forms";
 export * from "./Head";
 export * from "./Header";
