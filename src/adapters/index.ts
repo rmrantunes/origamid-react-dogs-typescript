f0d06d19d5f364445f9038e7c9da9f6f419705ca
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-14 08:32:40 -0400

    New fetch config: Reset password

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -2,5 +2,6 @@ export * from "./auth";
 export * from "./create-user";
 export * from "./get-photos";
 export * from "./post-photo";
+export * from "./reset-password";
 export * from "./response-definitions";
 export * from "./send-comment";
