d8a5a902ae50a893057dcbc24c8a0f80a5dfa083
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-14 11:20:30 -0400

    Fetch user stats in UserStats component

--- src/core/components/User/UserStats.tsx
+++ src/core/components/User/UserStats.tsx
@@ -1,10 +1,36 @@
-import { Head } from "src/core/components";
+import { useContext, useEffect } from "react";
+import { GET_STATS_FETCH_CONFIG, Stats } from "src/adapters";
+import {
+  Head,
+  Loading,
+  ErrorMessage,
+  UserStatsGraphs,
+} from "src/core/components";
+import { UserContext } from "src/core/contexts";
+import { useFetch } from "src/core/hooks";
 
 export const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      Estatísticas
-    </div>
-  );
+  const { token } = useContext(UserContext);
+  const { data: stats, error, loading, request } = useFetch<Stats[]>();
+
+  useEffect(() => {
+    async function getStatistics() {
+      if (!token) return;
+      const { url, options } = GET_STATS_FETCH_CONFIG(token);
+
+      await request(url, options);
+    }
+    getStatistics();
+  }, [request, token]);
+
+  if (loading) return <Loading />;
+  if (error) return <ErrorMessage {...{ error }} />;
+  if (stats)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs {...{ stats }} />
+      </div>
+    );
+  return <></>;
 };
